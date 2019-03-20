300fcebdd77c2e7297006c57100600fe6c940600
Daniel Anner <daniel.anner@danstechsupport.com>
2019-03-20 14:21:51 -0400

    appears to be working, running outputs

--- ProgrammingAssignments/Assignment 3/graph.py
+++ ProgrammingAssignments/Assignment 3/graph.py
@@ -3,6 +3,7 @@ import math
 import random
 from disjointsets import DisjointSets
 from pq import PQ
+import timeit
 
 # Programming Assignment 3
 # (5) After doing steps 1 through 4 below (look for relevant comments), return up here.
@@ -45,13 +46,13 @@ def generate_random_weighted_digraph(v,e,min_w,max_w) :
     # construct a Digraph with the lists of edges and weights generated
     G = Digraph(v, edges, weights)
     return G
-            
+
 
 
 
 def time_shortest_path_algs() :
     """Generates a table of timing results comparing two versions of Dijkstra's algorithm."""
-    
+
     # Programming Assignment 3
     #
     # (3) Make sure you find steps 1 and 2 later in this module (in this file in the Digraph class) then
@@ -75,7 +76,7 @@ def time_shortest_path_algs() :
     #     -- Repeat this again for 16 vertices and 32 edges.
     #     -- Repeat yet again with 64 vertices and 128 edges.
     #     -- Repeat yet again with 256 vertices and 512 edges.
-    #    
+    #
     #     -- Have this function output the timing data in a table, with columns for number of vertices, number of edges, and time.
     #     -- If you want, you can include larger digraphs.
     #        The pattern I used when indicating what size to use:
@@ -85,9 +86,151 @@ def time_shortest_path_algs() :
     #        For example, if you want to continue the experimentation with larger graphs, you might
     #        try 1024 vertices with 1047552 edges (dense graph), 1024 vertices with 2048 edges (sparse),
     #        and 1024 vertices with 104755 edges (somewhere between dense and sparse).
+    g_1632 = generate_random_weighted_digraph(16, 32, 1, 10)
+    g_1660 = generate_random_weighted_digraph(16, 60, 1, 10)
+    g_16240 = generate_random_weighted_digraph(16, 240, 1, 10)
+
+    g_64128 = generate_random_weighted_digraph(64, 128, 1, 10)
+    g_64672 = generate_random_weighted_digraph(64, 672, 1, 10)
+    g_644032 = generate_random_weighted_digraph(64, 4032, 1, 10)
+
+    g_256512 = generate_random_weighted_digraph(256, 512, 1, 10)
+    g_2568160 = generate_random_weighted_digraph(256, 8160, 1, 10)
+    g_25665280 = generate_random_weighted_digraph(256, 65280, 1, 10)
+
+    ### Graphs for Extra testing for 512 as the base
+    #g_5121024 = generate_random_weighted_digraph(512, 1024, 1, 10)
+    #g_51296569 = generate_random_weighted_digraph(512, 96569, 1, 10)
+    #g_512261632 = generate_random_weighted_digraph(512, 261632, 1, 10)
+
+    # Definitions for the timeit function - easiest way I could think of
+    # Graph 16-32
+    def graph_1632v1():
+        g_1632.bellman_ford(0)
+
+    def graph_1632v2():
+        g_1632.dijkstra(0)
+
+    # Graph 16-60
+    def graph_1660v1():
+        g_1660.bellman_ford(0)
+
+    def graph_1660v2():
+        g_1660.dijkstra(0)
+
+    # Graph 16-240
+    def graph_16240v1():
+        g_16240.bellman_ford(0)
+
+    def graph_16240v2():
+        g_16240.dijkstra(0)
+
+    # Graph 64-128
+    def graph_64128v1():
+        g_64128.bellman_ford(0)
+
+    def graph_64128v2():
+        g_64128.dijkstra(0)
+
+    # Graph 64-672
+    def graph_64672v1():
+        g_64672.bellman_ford(0)
+
+    def graph_64672v2():
+        g_64672.dijkstra(0)
+
+    # Graph 64-4032
+    def graph_644032v1():
+        g_644032.bellman_ford(0)
+
+    def graph_644032v2():
+        g_644032.dijkstra(0)
+
+    # Graph 256-512
+    def graph_256512v1():
+        g_256512.bellman_ford(0)
+
+    def graph_256512v2():
+        g_256512.dijkstra(0)
+
+    # Graph 256-8160
+    def graph_2568160v1():
+        g_2568160.bellman_ford(0)
+
+    def graph_2568160v2():
+        g_2568160.dijkstra(0)
+
+    # Graph 256-65280
+    def graph_25665280v1():
+        g_25665280.bellman_ford(0)
+
+    def graph_25665280v2():
+        g_25665280.dijkstra(0)
+
+    ### Functions for Extra testing for 512 as the base
+    # Graph 512-1024
+    #def graph_5121024v1():
+    #    g_5121024.bellman_ford(0)
+
+    #def graph_5121024v2():
+    #    g_5121024.dijkstra(0)
+
+    # Graph 512-96569
+    #def graph_51296569v1():
+    #    g_51296569.bellman_ford(0)
+
+    #def graph_51296569v2():
+    #    g_51296569.dijkstra(0)
+
+    # Graph 512-261632
+    #def graph_512261632v1():
+    #    g_512261632.bellman_ford(0)
+
+    #def graph_512261632v2():
+    #    g_512261632.dijkstra(0)
+
+
+    # Begin Printout Formatting
+    print("%0s %20s %10s %10s" % ("Version", "Vertices", "Edges", "Time"))
+    print('-' * 53)
+
+    print("%0s %10s %13s %16s" % ("Bellman Ford", "16", "32", "%.5f" % timeit.timeit(graph_1632v1, number=10000)))
+    print("%0s %10s %13s %16s" % ("Dijkstra    ", "16", "32", "%.5f" % timeit.timeit(graph_1632v2, number=10000)))
+
+    print("%0s %10s %13s %16s" % ("Bellman Ford", "16", "60", "%.5f" % timeit.timeit(graph_1660v1, number=10000)))
+    print("%0s %10s %13s %16s" % ("Dijkstra    ", "16", "60", "%.5f" % timeit.timeit(graph_1660v2, number=10000)))
+
+    print("%0s %10s %13s %16s" % ("Bellman Ford", "16", "240", "%.5f" % timeit.timeit(graph_16240v1, number=10000)))
+    print("%0s %10s %13s %16s" % ("Dijkstra    ", "16", "240", "%.5f" % timeit.timeit(graph_16240v2, number=10000)))
+
+    print("%0s %10s %13s %16s" % ("Bellman Ford", "64", "128", "%.5f" % timeit.timeit(graph_64128v1, number=10000)))
+    print("%0s %10s %13s %16s" % ("Dijkstra    ", "64", "128", "%.5f" % timeit.timeit(graph_64128v2, number=10000)))
+
+    print("%0s %10s %13s %16s" % ("Bellman Ford", "64", "672", "%.5f" % timeit.timeit(graph_64672v1, number=10000)))
+    print("%0s %10s %13s %16s" % ("Dijkstra    ", "64", "672", "%.5f" % timeit.timeit(graph_64672v2, number=10000)))
+
+    print("%0s %10s %13s %16s" % ("Bellman Ford", "64", "4032", "%.5f" % timeit.timeit(graph_644032v1, number=10000)))
+    print("%0s %10s %13s %16s" % ("Dijkstra    ", "64", "4032", "%.5f" % timeit.timeit(graph_644032v2, number=10000)))
+
+    print("%0s %10s %13s %16s" % ("Bellman Ford", "256", "512", "%.5f" % timeit.timeit(graph_256512v1, number=10000)))
+    print("%0s %10s %13s %16s" % ("Dijkstra    ", "256", "512", "%.5f" % timeit.timeit(graph_256512v2, number=10000)))
+
+    print("%0s %10s %13s %16s" % ("Bellman Ford", "256", "8160", "%.5f" % timeit.timeit(graph_2568160v1, number=10000)))
+    print("%0s %10s %13s %16s" % ("Dijkstra    ", "256", "8160", "%.5f" % timeit.timeit(graph_2568160v2, number=10000)))
+
+    print("%0s %10s %13s %16s" % ("Bellman Ford", "256", "65280", "%.5f" % timeit.timeit(graph_25665280v1, number=10000)))
+    print("%0s %10s %13s %16s" % ("Dijkstra    ", "256", "65280", "%.5f" % timeit.timeit(graph_25665280v2, number=10000)))
+
+    ### Prints for Extra testing for 512 as the base
+    #print("%0s %10s %13s %16s" % ("Bellman Ford", "512", "1024", "%.5f" % timeit.timeit(graph_5121024v1, number=10000)))
+    #print("%0s %10s %13s %16s" % ("Dijkstra    ", "512", "1024", "%.5f" % timeit.timeit(graph_5121024v2, number=10000)))
+
+    #print("%0s %10s %13s %16s" % ("Bellman Ford", "512", "96569", "%.5f" % timeit.timeit(graph_51296569v1, number=10000)))
+    #print("%0s %10s %13s %16s" % ("Dijkstra    ", "512", "96569", "%.5f" % timeit.timeit(graph_51296569v2, number=10000)))
+
+    #print("%0s %10s %13s %16s" % ("Bellman Ford", "512", "251632", "%.5f" % timeit.timeit(graph_512261632v1, number=10000)))
+    #print("%0s %10s %13s %16s" % ("Dijkstra    ", "512", "251632", "%.5f" % timeit.timeit(graph_512261632v2, number=10000)))
     pass
-
-
 
 class Graph :
     """Graph represented with adjacency lists."""
@@ -99,7 +242,7 @@ class Graph :
 
         Keyword arguments:
         v - number of vertices
-        edges - any iterable of ordered pairs indicating the edges 
+        edges - any iterable of ordered pairs indicating the edges
         weights - (optional) list of weights, same length as edges list
         """
 
@@ -126,11 +269,11 @@ class Graph :
 
         self._adj[a].add(b, w)
         self._adj[b].add(a, w)
-    
+
 
     def num_vertices(self) :
         """Gets number of vertices of graph."""
-        
+
         return len(self._adj)
 
 
@@ -140,7 +283,7 @@ class Graph :
         Keyword arguments:
         vertex - integer id of vertex
         """
-        
+
         return self._adj[vertex]._size
 
     def bfs(self, s) :
@@ -151,7 +294,7 @@ class Graph :
         Keyword arguments:
         s - the integer id of the starting vertex.
         """
-        
+
         class VertexData :
             __slots__ = [ 'd', 'pred' ]
 
@@ -208,7 +351,7 @@ class Graph :
 
     def print_graph(self, with_weights=False) :
         """Prints the graph."""
-        
+
         for v, vList in enumerate(self._adj) :
             print(v, end=" -> ")
             if with_weights :
@@ -231,7 +374,7 @@ class Graph :
         Keyword arguments:
         with_weights -- True to include weights
         """
-        
+
         edges = []
         for v, vList in enumerate(self._adj) :
             if with_weights :
@@ -247,7 +390,7 @@ class Graph :
         minimum spanning tree (MST) of the graph,
         computed using Kruskal's algorithm.
         """
-        
+
         A = set()
         forest = DisjointSets(len(self._adj))
         edges = self.get_edge_list(True)
@@ -299,7 +442,7 @@ class Digraph(Graph) :
     def add_edge(self, a, b, w=None) :
         self._adj[a].add(b, w)
 
-    
+
     def bellman_ford(self,s) :
         """Bellman Ford Algorithm for single source shortest path.
 
@@ -337,35 +480,54 @@ class Digraph(Graph) :
         #  When you get to part 2 later, when you implement Dijkstra's algorithm, you will need to do a similar
         #  thing (line 7 of the pseduocode of Dijkstra's algorithm).  Only there you'll just iterate over the
         #  vertices adjacent to u, and not all of the edges.
-        #
-        pass
+        dist = [float("Inf")] * len(self._adj) #init as infinite
+        dist[s] = 0
+
+        for v, vList in enumerate(self._adj):
+            for u, w in vList.__iter__(True) :
+                if dist[u] != float("Inf") and dist[u] + w < dist[v]: #update the value if not the infinite case and u + weight is less than v
+                    dist[v] = dist[u] + w
+
+        for v, vList in enumerate(self._adj):
+            for u, w in vList.__iter__(True) :
+                if dist[u] != float("Inf") and dist[u] + w < dist[v]: #check for negative weights, if so return empty
+                    return []
+                    
+        return dist
 
 
-    
+
     def dijkstra(self,s) :
         """Dijkstra's Algorithm using a binary heap as the PQ.
 
         Keyword Arguments:
         s - The source vertex.
         """
+        distance = [math.inf for x in self._adj]
+        parent = [None for x in self._adj]
+        thePQ = PQ()
+        distance[s] = 0
+        thePQ.add(s, 0)
+        search = []
+        for i in range(len(self._adj)):
+            if i != s:
+                thePQ.add(i, math.inf)
+        while not thePQ.is_empty():
+            i = thePQ.extract_min()
+            search.append(i)
+            for j, k in self._adj[i].__iter__(True):
+                if (distance[i] + k) < distance[j]:
+                    parent[j] = i
+                    distance[j] = (distance[i] + k)
+        tupleReturn = []
+        for j in search:
+            tupleReturn.append((j, distance[j], parent[j]))
+        return tupleReturn
 
-        # Programming Assignment 3:
-        # 2) Implement Dijkstra's Algorithm using a binary heap implementation of a PQ as the PQ.
-        #    Specifically, use the implementation I have posted here: https://github.com/cicirello/PythonDataStructuresLibrary
-        #    Use the download link (if you simply click pq.py Github will just show you the source in a web browser with line numbers).
-        #
-        #    Have this method return a list of 3-tuples, one for each vertex, such that first position is vertex id,
-        #    second is distance from source vertex (i.e., what pseudocode from textbook refers to as v.d), and third
-        #    is the vertex's parent (what the textbook refers to as v.pi).  E.g., (2, 10, 5) would mean the shortest path
-        #    from s to 2 has weight 10, and vertex 2's parent is vertex 5.
-        #
-        #    the parameter s is the source vertex.
-        pass
 
-    
-     
 
-        
+
+
 
 
 class _AdjacencyList :
@@ -390,9 +552,9 @@ class _AdjacencyList :
         else :
             return _AdjListIter(self)
 
-    
 
-    
+
+
 
 class _AdjListNode :
 
@@ -403,7 +565,7 @@ class _AdjListNode :
         self._data = data
         self._w = w
 
-        
+
 
 class _AdjListIter :
 
@@ -448,7 +610,7 @@ class _AdjListIterWithWeights :
 
 
 if __name__ == "__main__" :
-        
+    time_shortest_path_algs()
     # here is where you will implement any code necessary to confirm that your
     # methods work correctly.
     # Code in this if block will only run if you run this module, and not if you load this module with
@@ -457,10 +619,3 @@ if __name__ == "__main__" :
     # (4) Call your time_shortest_path_algs() function here to output the results of step 3.
 
     pass
-    
-
-
-
-
-    
-
